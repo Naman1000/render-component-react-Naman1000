da7771eb36a24123a91a64aedc7d02800968323c
Naman1000 <91007344+Naman1000@users.noreply.github.com>
2025-09-05 20:19:45 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
-import asf from "../asf.jsx";
+import Write from "../Write.jsx";
 
 const App = () => {
   return (
     <div id="main">
-    <asf />
+    <Write />
     </div>
   )
 }
